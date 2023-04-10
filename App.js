8defeb35128484ab221215434fffd82923f46254
Felipe Morales Simões <100170616+FelipeMoralesdev@users.noreply.github.com>
2023-04-09 21:51:54 -0300

    start navigation

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+ 
+import { NavigationContainer } from '@react-navigation/native'
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import Contacts from './src/pages/Contacts/'
+import Information from './src/pages/Information/'
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <NavigationContainer>
+        <Drawer.Navigator>
+          <Drawer.Screen name="Contacts" component={Contacts}/>
+          <Drawer.Screen name="Information" component={Information}/>
+        </Drawer.Navigator>
+      </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
